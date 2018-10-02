06eef1857d44cb427a5d56aeeea60bc2fad5a7ea
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-10-02 18:46:57 +0200

    Update Speeds.java

--- Speeds.java
+++ Speeds.java
@@ -1,37 +1,35 @@
 
 
 public class Speeds extends SpeedUnits {
-    double value;
-    int unit;
-    double speed;
+    private double value;
+    private int unit;
+    private double speed;
 
     Speeds(double value, int unit) {
         this.value = value;
         this.unit = unit;
-        this.speed = speed(unit);
-
+        this.speed = speedMS(value, unit);
     }
 
     Speeds(double value, String unit) {
-        super();
-        this.speed = speed(super.getIndexOfUnit(unit));
+        this.value = value;
+        this.unit = getIndexOfUnit(unit);
+        this.speed = speedMS(value, this.unit);
     }
 
+    public String getNameOfUnit() {
+        return super.getNameOfUnit(this.unit);
+    }
 
-    public double speed(int unit) { //return speed in m/h
-        switch (unit) {
-            case 0:
-                return value * 1000;//kmh
-            case 1:
-                return value * 1609;//mph
-            case 2:
-                return value * 1852;//kh
-            case 3:
-                return value * 60;//ms
-            default:
-                return -1;
+    public double getValue() {
+        return value;
+    }
 
-        }
+    public int getUnit() {
+        return unit;
+    }
 
+    public double getSpeed() {
+        return speed;
     }
 }
