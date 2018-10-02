d3c0ce5e77a99277ef2b182a873294c2f9491e9f
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-10-02 18:47:26 +0200

    Update TimeUnits.java

--- TimeUnits.java
+++ TimeUnits.java
@@ -1,13 +1,4 @@
-public class TimeUnits {
-    int indexOfTime;
-    TimeUnits(){
-        indexOfTime = 2;
-    }
-
-    TimeUnits(String time) {
-        indexOfTime = getIndexOfUnit(time);
-    }
-
+abstract public class TimeUnits {
     public int getIndexOfUnit(String unit) {
         switch (unit) {
             case "h":
@@ -19,5 +10,19 @@ public class TimeUnits {
             default:
                 return -1;
         }
+    }
+    public double time(double value, int unit) { //return time in "s"
+        switch (unit) {
+            case 0:
+                return value * 3600;//h
+            case 1:
+                return value * 60;//m
+            case 2:
+                return value;//s
+            default:
+                return -1;
+
+        }
+
     }
 }
